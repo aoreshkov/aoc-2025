474b5255b5c1039e60f3d7f9c6a707447dd89305
Atanas Oreshkov <aoreshkov@gmail.com>
2025-12-14 11:41:53 +0200

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,52 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var start = 50
+        return input
+            .map { line -> line[0] to line.drop(1).toInt() }
+            .filter { instruction ->
+                if(instruction.first == 'R') start += instruction.second
+                else if(instruction.first == 'L') start -= instruction.second
+                while(start < 0) start += 100
+                while(start > 99) start -= 100
+                start == 0
+            }
+            .size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var start = 50
+        var nb = 0
+
+        input
+            .map { line -> line[0] to line.substring(1).toInt() }
+            .forEach { instruction ->
+            if(instruction.first == 'R') {
+                start += instruction.second
+                while(start > 99) {
+                    start -= 100
+                    nb++
+                }
+            }
+            else if(instruction.first == 'L') {
+                if(start == 0) nb--
+                start -= instruction.second
+                while(start < 0) {
+                    start += 100
+                    nb++
+                }
+                if(start == 0) nb++
+            }
+        }
+        return nb
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
+
+    check(part2(testInput) == 6)
+
     part2(input).println()
 }
